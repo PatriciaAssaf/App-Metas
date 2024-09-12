db5adeccc7449c4aa2da7fe1d456c10b127ac0af
Patricia Assaf <assafpati@gmail.com>
2024-09-12 16:00:01 -0300

    adiciona metas abertas

--- index.js
+++ index.js
@@ -61,8 +61,24 @@ const metasRealizadas = async () => {
     }
 
     await select ({
-        message: "Metas Realizadas",
+        message: "Metas Realizadas " + realizadas.length,
         choices: [...realizadas]
+    })
+}
+
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+        return meta.checked != true 
+    })
+
+    if (abertas.length == 0) {
+        console.log ("Não existem metas abertas! :(")
+        return
+    }
+
+    await select ({
+        message: "Metas Abertas " + abertas.length,
+        choices: [...abertas]
     })
 }
 
@@ -86,6 +102,10 @@ const start = async () => {
                     value: "realizadas"
                 },
                 {
+                    name: "Metas abertas",
+                    value: "abertas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -103,6 +123,9 @@ const start = async () => {
             break
         case "realizadas":
             await metasRealizadas()
+            break
+        case "abertas":
+            await metasAbertas()
             break
         case "sair":
             console.log("Até a próxima!")
--- todo.md
+++ todo.md
@@ -1,7 +1,7 @@
 - [x] Criar Metas
 - [x] Listar Metas
    [x] - Metas realizadas
-    - Metas abertas
+   [x] - Metas abertas
 - [x] Marcar/Desmarcar Metas Realizadas
 - Remover Metas
 - Sistema de Mensagens
